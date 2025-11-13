5b8a68f9c9cc30a62fb70bae679497681842f4c8
eneekoruiz <eruiz084@ikasle.ehu.eus>
2025-11-13 10:51:52 +0100

    Mileston1

---
+++ js/main.js
@@ -0,0 +1,68 @@
+import { folder, leftArrow } from "./fragments.js";
+import { fetchJSON } from "./loaders.js";
+
+function differenceInDays(date1) {
+    // YOUR CODE HERE
+    const hoy = new Date();
+    if(date1 > hoy){
+        console.log("data txikixaua")
+        return 0;
+    }
+    const differenceInTime = hoy.getTime() - date1.getTime();
+    console.log(differenceInTime);
+    const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+    return differenceInDays;
+}
+
+let difference_In_Days = differenceInDays(new Date("01-10-2025")); //"01-10-2025"
+
+window.onload = function () {
+  document.getElementById("gamenumber").innerText = difference_In_Days.toString();
+  document.getElementById("back-icon").innerHTML = folder + leftArrow;
+};
+
+let game = {
+  guesses: [],
+  solution: {},
+  players: [],
+  leagues: []
+};
+
+function getSolution(players, solutionArray, difference_In_Days) {
+    // YOUR CODE HERE
+    let index = (difference_In_Days - 1) % solutionArray.length;
+    //let index = Math.abs(difference_In_Days % solutionArray.length);
+
+    console.log("Gaurko indizea"+ index);
+    let solutionId = solutionArray[index];
+    console.log("gaurko indizea"+solutionId);
+    console.log(players);
+    let son = players.find(player => player.id === Number(solutionId));
+
+    if (!son) {
+        console.log("Ez da aurkitu");
+    } else {
+        console.log("Aurkitu da:", son);
+    }
+    return son;
+    //console.log(players.find(player => player.id === solutionId))
+    //return players.find(player => player.id === solutionId);
+
+}
+
+Promise.all([fetchJSON("fullplayers25"), fetchJSON("solution25")]).then(
+  (values) => {
+
+    let solution;
+    
+    [game.players, solution] = values;
+    console.log("✅ Players:", game.players);
+    console.log("✅ Solution array:", solution);
+    game.solution = getSolution(game.players, solution, difference_In_Days);
+    
+    console.log(game.solution);
+
+    document.getElementById("mistery").src = `https://playfootball.games/media/players/${game.solution.id % 32}/${game.solution.id}.png`;
+  
+  }
+);
